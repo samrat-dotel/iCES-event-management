5d61fc732f7a6d41780b77b7478c6f0f8c69ff47
samrat-dotel <samratdotel7264@gmail.com>
2024-02-01 22:49:31 +0545

    initial home page

---
+++ src/datas/events.js
@@ -0,0 +1,76 @@
+const eventList = [
+  {
+    index: 1,
+    image: "/images/beach.png",
+    title: "Bootcamp",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    buttonText: "Learn More",
+    link: "/",
+  },
+  {
+    index: 2,
+    image: "/images/OIP.jpeg",
+    title: "Bootcamp",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    buttonText: "Learn More",
+    link: "/",
+  },
+  {
+    index: 3,
+    image: "/images/beach.png",
+    title: "Bootcamp",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    buttonText: "Learn More",
+    link: "/",
+  },
+  {
+    index: 4,
+    image: "/images/OIP.jpeg",
+    title: "Bootcamp",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    buttonText: "Learn More",
+    link: "/",
+  },
+  {
+    index: 5,
+    image: "/images/beach.png",
+    title: "Bootcamp",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    buttonText: "Learn More",
+    link: "/",
+  },
+  {
+    index: 6,
+    image: "/images/OIP.jpeg",
+    title: "Bootcamp",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    buttonText: "Learn More",
+    link: "/",
+  },
+  {
+    index: 7,
+    image: "/images/beach.png",
+    title: "Bootcamp",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    buttonText: "Learn More",
+    link: "/",
+  },
+  {
+    index: 8,
+    image: "/images/beach.png",
+    title: "Bootcamp",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    buttonText: "Learn More",
+    link: "/",
+  },
+];
+
+export default eventList;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,73 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import Navbar from "../../components/Navbar";
+import eventList from "../../datas/events";
+import styles from "../Home/Home.module.css";
 
 const Home = () => {
+  const eventsPerPage = 5;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const indexOfLastEvent = currentPage * eventsPerPage;
+  const indexOfFirstEvent = indexOfLastEvent - eventsPerPage;
+  const currentEvents = eventList.slice(indexOfFirstEvent, indexOfLastEvent);
+
+  const totalPages = Math.ceil(eventList.length / eventsPerPage);
+
+  const nextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const prevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
   return (
     <section>
       <Navbar />
-      <div>
-        <p>Hello How do u do?</p>
+      <div className={styles.main}>
+        <p className={styles.pageTitle}>Upcoming Events</p>
+
+        <div className={styles.eventList}>
+          {currentEvents.map((data, index) => (
+            <div key={data.title} className={styles.eventBox}>
+              <p className={styles.title}>
+                {indexOfFirstEvent + index + 1}. {data.title}
+              </p>
+
+              <div className={styles.boxContents}>
+                <img
+                  src={data.image}
+                  alt={data.title}
+                  width="400"
+                  height="300"
+                />
+
+                <div className={styles.cardContents}>
+                  <p className={styles.description}>{data.description}</p>
+                  <Link className={styles.button} to={data.link}>
+                    {data.buttonText}
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className={styles.pagination}>
+          <button onClick={prevPage} disabled={currentPage === 1}>
+            Previous
+          </button>
+          <span className={styles.pageNumber}>
+            {currentPage} of {totalPages}
+          </span>
+          <button onClick={nextPage} disabled={currentPage === totalPages}>
+            Next
+          </button>
+        </div>
       </div>
     </section>
   );
