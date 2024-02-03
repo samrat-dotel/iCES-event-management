96f151b52aab30ff6ab0b79801a2f2e7b08f2923
samrat-dotel <samratdotel7264@gmail.com>
2024-02-03 22:44:02 +0545

    add some components in the event detail page

--- src/pages/Event/Event.jsx
+++ src/pages/Event/Event.jsx
@@ -18,7 +18,7 @@ const Event = () => {
       <Navbar />
       <div className={styles.main}>
         <div className={styles.container}>
-          <div className={styles.texts}>
+          <div className={styles.contents}>
             <img
               src={selectedEvent.image}
               alt={selectedEvent.title}
@@ -28,6 +28,33 @@ const Event = () => {
             <p className={styles.title}>{selectedEvent.title}</p>
           </div>
           <p className={styles.description}>{selectedEvent.description}</p>
+          <div className={styles.specials}>
+            <p className={styles.text}>
+              Event coordinator:{" "}
+              <span className={styles.elements}>
+                {" "}
+                {selectedEvent.coordinator}
+              </span>
+            </p>
+            <p className={styles.text}>
+              Event sub-coordinator:{" "}
+              <span className={styles.elements}>
+                {selectedEvent.subcoordinator}
+              </span>
+            </p>
+            <p className={styles.text}>
+              Start date:{" "}
+              <span className={styles.elements}>{selectedEvent.startDate}</span>
+            </p>
+            <p className={styles.text}>
+              End date:{" "}
+              <span className={styles.elements}>{selectedEvent.endDate}</span>
+            </p>
+            <p className={styles.text}>
+              Time:{" "}
+              <span className={styles.elements}>{selectedEvent.time}</span>
+            </p>
+          </div>
           <Link className={styles.button} type="button" to="/">
             Register now
           </Link>
