73a346733471f368ab5ddba22fd559511c4e215d
samrat-dotel <samratdotel7264@gmail.com>
2024-02-04 13:38:08 +0545

    css fix for admin form and event page

--- src/pages/Event/Event.jsx
+++ src/pages/Event/Event.jsx
@@ -28,40 +28,44 @@ const Event = () => {
             <p className={styles.title}>{selectedEvent.title}</p>
           </div>
           <p className={styles.description}>{selectedEvent.description}</p>
-          <div className={styles.specials}>
-            <p className={styles.text}>
-              Event coordinator:{" "}
-              <span className={styles.elements}>
-                {" "}
-                {selectedEvent.coordinator}
-              </span>
-            </p>
-            <p className={styles.text}>
-              Event sub-coordinator:{" "}
-              <span className={styles.elements}>
-                {selectedEvent.subcoordinator}
-              </span>
-            </p>
-            <p className={styles.text}>
-              Start date:{" "}
-              <span className={styles.elements}>{selectedEvent.startDate}</span>
-            </p>
-            <p className={styles.text}>
-              End date:{" "}
-              <span className={styles.elements}>{selectedEvent.endDate}</span>
-            </p>
-            <p className={styles.text}>
-              Time:{" "}
-              <span className={styles.elements}>{selectedEvent.time}</span>
-            </p>
-            <p className={styles.text}>
-              Venue:{" "}
-              <span className={styles.elements}>{selectedEvent.venue}</span>
-            </p>
+          <div className={styles.specialBox}>
+            <div className={styles.specials}>
+              <p className={styles.text}>
+                <span className={styles.tag}>Event coordinator: </span>
+                <span className={styles.elements}>
+                  {" "}
+                  {selectedEvent.coordinator}
+                </span>
+              </p>
+              <p className={styles.text}>
+                <span className={styles.tag}>Event sub-coordinator: </span>
+                <span className={styles.elements}>
+                  {selectedEvent.subcoordinator}
+                </span>
+              </p>
+              <p className={styles.text}>
+                <span className={styles.tag}>Start date: </span>
+                <span className={styles.elements}>
+                  {selectedEvent.startDate}
+                </span>
+              </p>
+              <p className={styles.text}>
+                <span className={styles.tag}>End date: </span>
+                <span className={styles.elements}>{selectedEvent.endDate}</span>
+              </p>
+              <p className={styles.text}>
+                <span className={styles.tag}>Time: </span>
+                <span className={styles.elements}>{selectedEvent.time}</span>
+              </p>
+              <p className={styles.text}>
+                <span className={styles.tag}>Venue: </span>
+                <span className={styles.elements}>{selectedEvent.venue}</span>
+              </p>
+            </div>
+            <Link className={styles.button} type="button" to="/">
+              Register now
+            </Link>
           </div>
-          <Link className={styles.button} type="button" to="/">
-            Register now
-          </Link>
         </div>
       </div>
     </section>
