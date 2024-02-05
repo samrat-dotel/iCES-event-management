4803834eb3e755618f5727a906c249ce595c3e99
samrat-dotel <samratdotel7264@gmail.com>
2024-02-05 10:33:16 +0545

    create ongoing event and attendance folders

---
+++ src/pages/Attendance/Attendance.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../Attendance/Attendance.module.css";
+
+const Attendance = () => {
+  return <div>Attendance</div>;
+};
+
+export default Attendance;
---
+++ src/pages/Home/OngoingEvents/OnngoingEvent.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../OngoingEvents/OngoingEvent.module.css";
+
+const OnngoingEvent = () => {
+  return <div>OnngoingEvent</div>;
+};
+
+export default OnngoingEvent;
